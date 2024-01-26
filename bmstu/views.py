18faba8178c6a1a9675751aad223957e52bf25de
VadimDzitiev <116982653+VadimDzitiev@users.noreply.github.com>
2024-01-26 04:02:26 +0300

    Update views.py

--- bmstu/views.py
+++ bmstu/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 data = {'transitions': [
-            {'from': 'МТ', 'to': 'Главный кампус', 'id': 1, 'transition': 'Маршрут из МТ в Главное здание', 'img_url': '/pictures/МТ-ГЗ.png','transition_img': '/pictures/МТ-ГЗ Маршрут.PNG'},
-            {'from': 'Главный кампус', 'to':'Энерго','id': 2, 'transition':'Маршрут из Главного здания в Энерго','img_url':'/pictures/ГЗ-Э.png','transition_img':'/pictures/ГЗ-Э Маршрут.png'},
-            {'from': 'Энерго', 'to':'СМ','id': 3, 'transition':'Маршрут из Энерго в СМ','img_url':'/pictures/Э-СМ.png','transition_img':'/pictures/Э-СМ Маршрут.PNG'},
-            {'from': 'СМ', 'to':'УЛК','id': 4, 'transition':'Маршрут из СМ в УЛК','img_url':'/pictures/СМ-УЛК.png','transition_img':'/pictures/СМ-УЛК Маршрут.PNG'},
-            {'from': 'УЛК', 'to':'СК','id': 5, 'transition':'Маршрут из УЛК в СК','img_url':'/pictures/УЛК-СК.png','transition_img':'/pictures/УЛК-СК Маршрут.PNG'}]}
+            {'from': 'МТ', 'to': 'Главный кампус', 'id': 1, 'transition_time':8,'transition': 'Маршрут из МТ в Главное здание', 'img_url': '/pictures/МТ-ГЗ.png','transition_img': '/pictures/МТ-ГЗ Маршрут.PNG'},
+            {'from': 'Главный кампус', 'to':'Энерго','id': 2, 'transition_time':4,'transition':'Маршрут из Главного здания в Энерго','img_url':'/pictures/ГЗ-Э.png','transition_img':'/pictures/ГЗ-Э Маршрут.png'},
+            {'from': 'Энерго', 'to':'СМ','id': 3, 'transition_time':2,'transition':'Маршрут из Энерго в СМ','img_url':'/pictures/Э-СМ.png','transition_img':'/pictures/Э-СМ Маршрут.PNG'},
+            {'from': 'СМ', 'to':'УЛК','id': 4, 'transition_time':5,'transition':'Маршрут из СМ в УЛК','img_url':'/pictures/СМ-УЛК.png','transition_img':'/pictures/СМ-УЛК Маршрут.PNG'},
+            {'from': 'УЛК', 'to':'СК','id': 5, 'transition_time':15,'transition':'Маршрут из УЛК в СК','img_url':'/pictures/УЛК-СК.png','transition_img':'/pictures/УЛК-СК Маршрут.PNG'}]}
 
 def GetCampus(request, id):
     new_data={}
